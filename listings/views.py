b064cb1b8c248ba53acda6d0d05a216c04c36d3c
Shawn Lin <shawnxiaolin@gmail.com>
2016-11-27 15:56:17 +1300

    Setup index listings page

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import TNZRegion
 
 # Create your views here.
+
+
+class Index(generic.ListView):
+    template_name = 'listings/index.html'
+    model = TNZRegion
