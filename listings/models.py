49566d18e7eacabaeea881512ee41b9028d42e4f
Shawn Lin <shawnxiaolin@gmail.com>
2016-11-28 22:38:39 +1300

    Handle exeption

--- listings/models.py
+++ listings/models.py
@@ -82,13 +82,13 @@ class TNZListing(models.Model):
     def get_main_image(self):
         try:
             image_instances = next(iter(self.main_image.values()))['instances']
-        except IndexError:
-            return ''
+        except (IndexError, AttributeError):
+            return {'url': ''}
         else:
             try:
                 original_image = [instance for instance in image_instances if instance['format'] == 'original'][0]
             except IndexError:
-                return ''
+                return {'url': ''}
             else:
                 return original_image
 
