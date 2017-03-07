07d114f53f200aaacac218a8ccc6b516e0b6cf6d
Shawn Lin <shawnxiaolin@gmail.com>
2017-03-07 21:19:17 +1300

    Contact form and email sending.

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,61 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+
+from .forms import ContactForm
 
 
 def index(request):
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            try:
+                submit(form.cleaned_data)
+            except(TypeError, ValueError):
+                return render(request, 'contact/contact.html', {
+                    'form': form,
+                    'message': '发生点小意外，请稍后再试。',
+                    'message_type': 'bad'
+                })
+            else:
+                return render(request, 'contact/contact.html', {
+                    'message': '我们已经收到您的信息，感谢与我们联系。',
+                    'message_type': 'good'
+                })
+        else:
+            return render(request, 'contact/contact.html', {
+                'form': form,
+                'message': '请正确填写所需信息。',
+                'message_type': 'bad'
+            })
+    else:
+        form = ContactForm()
+        return render(request, 'contact/contact.html', {'form': form})
 
 
-def submit(request):
-    data = request.POST
-    print(data)
-    return render(request, 'contact/contact.html', {
-        'message': '我们已经收到您的消息，感谢与我们联系。',
-        'message_type': 'good'
+def submit(data):
+    email = data.get('email')
+    subject = data.get('subject')
+    message = data.get('message')
+    name = data.get('name')
+    text_content = 'Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}\n'\
+        .format(name=name, email=email, subject=subject, message=message)
+    template = get_template('contact/email.html')
+    context = Context({
+        'subject': subject,
+        'message': message,
+        'name': name,
+        'email': email,
     })
+    html_content = template.render(context)
+    email = EmailMultiAlternatives(
+        'FantasyNZ contact form submission',
+        text_content,
+        'no-reply@fantasynz.com',
+        ['shawnxiaolin@gmail.com'],
+        headers={'Reply-To': data.get('email')}
+    )
+    email.attach_alternative(html_content, 'text/html')
+    email.send(fail_silently=True)
+
--- fantasy/settings/production.py
+++ fantasy/settings/production.py
@@ -4,6 +4,15 @@ from .base import *
 
 DEBUG = False
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'Fantasy NZ <no-reply@fantasynz.com>'
+
+
 ALLOWED_HOSTS = [
     'www.fantasynz.com'
 ]
