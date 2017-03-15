ca750cec8af67a5d7bf63d1c6c34de87452ed506
Shawn Lin <shawnxiaolin@gmail.com>
2017-03-15 20:48:02 +1300

    Region name and business types json feed.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    url(r'^listings/$', views.listings, name='listings')
+    url(r'^listings/$', views.listings, name='listings'),
+    url(r'^regions/$', views.regions, name='regions'),
+    url(r'^types/$', views.types, name='types'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
+import json
+
 from django.http import HttpResponse
+from django.core.serializers import serialize
+from django.db.models import Count
 
 from listings.views import ListingsFilter
+from listings.models import TNZListing
 
 
 def listings(request):
@@ -22,3 +27,27 @@ def sanitize_filters(raw_filters):
     :return:
     """
     return raw_filters
+
+
+def regions(request):
+    """
+    Get all the regionname from listings and aggregate listings count.
+    :param request:
+    :return:
+    """
+    regions_data = TNZListing.objects.values('regionname').annotate(listings_count=Count('id'))
+    response = HttpResponse(json.dumps({'data': list(regions_data)}))
+    response.__setitem__('Content-Type', 'application/json')
+    return response
+
+
+def types(request):
+    """
+    Get all the business types from listings and aggregate listings count.
+    :param request:
+    :return:
+    """
+    types_data = TNZListing.objects.values('business_type').annotate(listings_count=Count('id'))
+    response = HttpResponse(json.dumps({'data': list(types_data)}))
+    response.__setitem__('Content-Type', 'application/json')
+    return response
